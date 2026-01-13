d25827ff2012f4864a8329192fd8b5efe5cb2ed4
Audchyyy <sitthisak30524@gmail.com>
2026-01-13 10:33:14 +0700

    second_main.py

--- main.py
+++ main.py
@@ -1,38 +1,44 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = b'ORCLORD'
-#app.secret_key = b'ORCLORD'
+app.config['SECRET_KEY'] = b'khiopgfdfghj;oioiouiy'
 
 cars = [
-    {'id':1, 'brand':'Toyota', 'model':'Yaris Ativ', 'year':2024, 'price':560000},
-    {'id':2, 'brand':'Toyota', 'model':'Yaris Cross', 'year':2025, 'price':790000},
-    {'id':3, 'brand':'Nissan', 'model':'Kicks', 'year':2024, 'price':800000}
-    
+    {'id': 1, 'brand': 'Toyota', 'model': 'Yaris Ativ', 'year': 2024, 'price': 560000},
+    {'id': 2, 'brand': 'Toyota', 'model': 'Yaris Cross', 'year': 2025, 'price': 790000},
+    {'id': 3, 'brand': 'Nissan', 'model': 'Kicks', 'year': 2024, 'price': 850000}
 ]
 
-@app.route("/")
+
+@app.route('/')
 def index():
     return render_template('index.html', title='Home Page')
 
-@app.route('/cars',methods=['GET', 'POST'])
+
+@app.route('/cars', methods=['GET', 'POST'])
 def show_cars():
     if request.method == 'POST':
         brand = request.form['brand']
         tmp_cars = []
 
         for car in cars:
-            if brand in car['brand']:
+            if brand.lower() in car['brand'].lower():
                 tmp_cars.append(car)
-            return render_template('cars/cars.html',
-                            title='Show Car By Brand Page',
-                            cars=tmp_cars)
-        
-    return render_template('cars/cars.html',
-                            title='Show All Cars Page',
-                            cars=cars)
 
-@app.route('/cars/new', methods=['GET','POST'])
+        return render_template(
+            'cars/cars.html',
+            title='Show Cars by Brand Page',
+            cars=tmp_cars
+        )
+
+    return render_template(
+        'cars/cars.html',
+        title='Show All Cars Page',
+        cars=cars
+    )
+
+
+@app.route('/cars/new', methods=['GET', 'POST'])
 def new_car():
     if request.method == 'POST':
         brand = request.form['brand']
@@ -40,18 +46,28 @@ def new_car():
         year = int(request.form['year'])
         price = int(request.form['price'])
 
-        length = len(cars)
-        if length != 0:
-            id = cars[length-1]['id'] + 1
+        if len(cars) != 0:
+            id = cars[-1]['id'] + 1
         else:
-            id = 1 
-        car = {'id': id, 'brand': brand, 'model': model, 'year': year, 'price': price}
+            id = 1
+
+        car = {
+            'id': id,
+            'brand': brand,
+            'model': model,
+            'year': year,
+            'price': price
+        }
 
         cars.append(car)
         flash('Add new car successfully', 'success')
         return redirect(url_for('show_cars'))
 
-    return render_template('cars/new_car.html', title='New Car Page')
+    return render_template(
+        'cars/new_car.html',
+        title='New Car Page'
+    )
+
 
 @app.route('/cars/<int:id>/delete')
 def delete_car(id):
@@ -60,37 +76,32 @@ def delete_car(id):
             cars.remove(car)
             break
 
-    flash('Delete car successfully','success')
+    flash('Delete car successfully', 'success')
     return redirect(url_for('show_cars'))
 
 
-@app.route('/cars/<int:id>/edit', methods=['GET','POST'])
+@app.route('/cars/<int:id>/edit', methods=['GET', 'POST'])
 def edit_car(id):
+    car = None
     for c in cars:
         if id == c['id']:
             car = c
             break
 
-    if request.method == 'POST':
-        brand = request.form['brand']
-        model = request.form['model']
-        year = int(request.form['year'])
-        price = int(request.form['price'])
+    if car is None:
+        return "Car not found", 404
 
-        for car in cars :
-            if id == car['id'] :
-                car['brand'] = brand
-                car['model'] = model
-                car['year'] = year
-                car['price'] = price
-                break
+    if request.method == 'POST':
+        car['brand'] = request.form['brand']
+        car['model'] = request.form['model']
+        car['year'] = int(request.form['year'])
+        car['price'] = int(request.form['price'])
 
         flash('Update car successfully', 'success')
         return redirect(url_for('show_cars'))
-    
+
     return render_template(
         'cars/edit_car.html',
         title='Edit Car Page',
         car=car
     )
-
